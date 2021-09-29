52af0de87313ef09c407c737409c8b78b563f2ec
Kamenskiy Andrey <87336070+LeDron12@users.noreply.github.com>
2021-09-29 18:41:30 +0300

    added some secondary functionality
    now translated program has printing for each variable to compare execution with original one

---
+++ kam/src/cmake-build-debug/CMakeFiles/c2eo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c2eo.dir/analyzers.cpp.o"
+  "CMakeFiles/c2eo.dir/funcdecl.cpp.o"
+  "CMakeFiles/c2eo.dir/generator.cpp.o"
+  "CMakeFiles/c2eo.dir/main.cpp.o"
+  "CMakeFiles/c2eo.dir/matchers.cpp.o"
+  "CMakeFiles/c2eo.dir/util.cpp.o"
+  "CMakeFiles/c2eo.dir/vardecl.cpp.o"
+  "c2eo"
+  "c2eo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c2eo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
